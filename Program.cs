37c674a534b6d781270352d14001bcc105b860e3
Gulshanoh <47572905+Gulshanoh@users.noreply.github.com>
2019-02-19 19:31:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1902
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter 2 numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a>b)
+            {
+
+                Console.WriteLine("the first number its bigger");
+            }
+
+        else
+            {
+                Console.WriteLine("the second number its bigger");
+            
+            }
+        }
+    }
+}
